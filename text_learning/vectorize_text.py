532288caf2e00a84eacdb7719593025803b0fc9f
Aziz Mamatov <amamatov@yahoo.com>
2017-05-31 16:02:49 -0700

    Update vectorize_text.py

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -55,7 +55,21 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             ### append the text to word_data
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            replace = ["sara", "shackleton", "chris", "germani"]
+            content = parseOutText(email)
+            ### use str.replace() to remove any instances of the words
+            ### ["sara", "shackleton", "chris", "germani"]
+            for w in replace:
+                content = content.replace(w, '')
+            
+            ### append the text to word_data
+            word_data.append(content)
 
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
